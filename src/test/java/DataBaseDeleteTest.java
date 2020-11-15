c169f2f0d34f5b5128fa9f10d761a9d57825d8db
kostiantyn.doroshenko <ku4er007@gmail.com>
2020-11-15 16:05:03 +0200

    HW done

---
+++ src/test/java/DataBaseDeleteTest.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBaseDeleteTest {
+    public static void main(String[] args) {
+        Connection conn1 = null;
+
+        try {
+            String url1 = "jdbc:mysql://127.0.0.1:3306/schema1";
+            String user = "user";
+            String password = "user";
+
+            conn1 = DriverManager.getConnection(url1, user, password);
+            if (conn1 != null) {
+                System.out.println("Connected to the database test1");
+            }
+            Statement statement1 = conn1.createStatement();
+            statement1.executeUpdate("DELETE FROM  schema1.student WHERE id = 1;");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn1.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
